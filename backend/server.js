0097ecea762bfbc0d11ea653ae6e573dcdd8ff48
Kaustubh <kaustubhthakur76@gmail.com>
2025-03-08 01:34:18 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,17 @@
 const express = require('express')
 const app = express();
+const port = 6969
+const mongoose = require('mongoose')
+require('dotenv').config();
+const connection = async()=>{
+    try {
+   await mongoose.connect(process.env.MONGODB)
+   console.log('db is connected...')
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+app.listen(port,() =>{
+console.log(`server is running on port ${port}`)
+})
