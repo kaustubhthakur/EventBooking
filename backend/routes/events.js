6504292f60851181be2803afd2168d2e530289ff
Kaustubh <kaustubhthakur76@gmail.com>
2025-03-13 22:20:56 +0530

    first commit

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,5 +1,10 @@
 const express  = require('express')
 const router = express.Router();
+const {createEvent,getEvents,deleteEvent} = require('../controllers/events')
+const protectRoute = require('../utils/protectRoute')
+const verifyUser = require('../utils/organizer')
 
-
+router.get('/',verifyUser,getEvents)
+router.post('/',protectRoute,verifyUser,createEvent)
+router.delete('/:id',protectRoute,verifyUser,deleteEvent)
 module.exports = router;
